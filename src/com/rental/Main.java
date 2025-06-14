7c2f8cab06581b0ddc2916a805106c54e18c2acf
Shravan Kumar Nunsavath <93977456+Nshravankumar4@users.noreply.github.com>
2025-06-14 14:43:45 +0530

    Update Main.java

--- src/com/rental/Main.java
+++ src/com/rental/Main.java
@@ -1,1 +1,20 @@
+package com.rental;
+
+public class Main {
+    public static void main(String[] args) {
+        RentalService service = new RentalService();
+
+        service.addVehicle(new Car("C101", "Toyota Camry", 60));
+        service.addVehicle(new Bike("B202", "Yamaha R15", 20));
+        service.addVehicle(new Truck("T303", "Volvo Truck", 100));
+
+        service.listAvailableVehicles();
+
+        service.rentVehicle("C101", 3);
+        service.rentVehicle("B202", 2);
+        service.returnVehicle("B202");
+
+        service.listAvailableVehicles();
+    }
+}
 
