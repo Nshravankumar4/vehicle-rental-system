a4292754414efe97198c1e61cd317b162e13b2ec
Shravan Kumar Nunsavath <93977456+Nshravankumar4@users.noreply.github.com>
2025-06-14 14:42:27 +0530

    Update Car.java

--- src/com/rental/Car.java
+++ src/com/rental/Car.java
@@ -1,1 +1,31 @@
+package com.rental;
+
+public class Car extends Vehicle implements Rentable {
+    public Car(String vehicleID, String modelName, double rentalRate) {
+        super(vehicleID, modelName, rentalRate);
+    }
+
+    @Override
+    public double calculateRentalCost(int days) {
+        return rentalRate * days;
+    }
+
+    @Override
+    public boolean rentVehicle() {
+        if (!isRented) {
+            isRented = true;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean returnVehicle() {
+        if (isRented) {
+            isRented = false;
+            return true;
+        }
+        return false;
+    }
+}
 
